6a67e3e8d3dc4226bfb7582f92c412a24992efdf
Mason <misspog42@gmail.com>
2020-08-27 19:05:50 -0400

    Updates

---
+++ precache-manifest.2190c4b45652c06c303cdc0ebdfe8871.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ff761f75ffae5d3224ece3f538a76dc",
+    "url": "/Test/index.html"
+  },
+  {
+    "revision": "61655420f6ba6618b581",
+    "url": "/Test/static/css/main.b26353d0.chunk.css"
+  },
+  {
+    "revision": "16dd2e1a7ef40d78ea22",
+    "url": "/Test/static/js/2.27f24bf7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Test/static/js/2.27f24bf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61655420f6ba6618b581",
+    "url": "/Test/static/js/main.b7cea9ae.chunk.js"
+  },
+  {
+    "revision": "165cacee0dac35a2984b",
+    "url": "/Test/static/js/runtime-main.7bdb3c7b.js"
+  },
+  {
+    "revision": "004080d40583fa80101dcaa55fa4735d",
+    "url": "/Test/static/media/avatar.004080d4.jpg"
+  },
+  {
+    "revision": "282b6494772a6772d89c4b24e9609ae8",
+    "url": "/Test/static/media/linkedin.282b6494.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Test/static/media/logo.5d5d9eef.svg"
+  }
+]);
